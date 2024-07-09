0e30e07bc4073f1da974d5a0e1c76f1310f5626c
Jean-Sébastien Grenier <jgrenier0@hotmail.com>
2024-07-09 19:55:19 -0400

    Edited the context menu buttons

--- EZBeat/Controls/TrackCtxMenu.Designer.vb
+++ EZBeat/Controls/TrackCtxMenu.Designer.vb
@@ -36,9 +36,9 @@ Partial Class TrackCtxMenu
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Guna2Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(34, Byte), Integer))
         Me.Guna2Panel1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(59, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(59, Byte), Integer))
-        Me.Guna2Panel1.Controls.Add(Me.Guna2Separator1)
         Me.Guna2Panel1.Controls.Add(Me.Guna2Button3)
         Me.Guna2Panel1.Controls.Add(Me.Guna2Button1)
+        Me.Guna2Panel1.Controls.Add(Me.Guna2Separator1)
         Me.Guna2Panel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(34, Byte), Integer))
         Me.Guna2Panel1.Location = New System.Drawing.Point(1, 2)
         Me.Guna2Panel1.Name = "Guna2Panel1"
@@ -48,32 +48,31 @@ Partial Class TrackCtxMenu
         'Guna2Separator1
         '
         Me.Guna2Separator1.FillColor = System.Drawing.Color.FromArgb(CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer))
-        Me.Guna2Separator1.Location = New System.Drawing.Point(6, 34)
+        Me.Guna2Separator1.Location = New System.Drawing.Point(0, 34)
         Me.Guna2Separator1.Name = "Guna2Separator1"
-        Me.Guna2Separator1.Size = New System.Drawing.Size(177, 10)
+        Me.Guna2Separator1.Size = New System.Drawing.Size(189, 10)
         Me.Guna2Separator1.TabIndex = 6
         Me.Guna2Separator1.TabStop = False
         '
         'Guna2Button3
         '
-        Me.Guna2Button3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Guna2Button3.BorderRadius = 4
+        Me.Guna2Button3.Animated = True
         Me.Guna2Button3.CustomImages.Image = Global.EZBeat.My.Resources.Resources.icons8_download_24
         Me.Guna2Button3.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
         Me.Guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray
         Me.Guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
         Me.Guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
         Me.Guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button3.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.Guna2Button3.FillColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(34, Byte), Integer))
         Me.Guna2Button3.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2Button3.ForeColor = System.Drawing.Color.White
         Me.Guna2Button3.HoverState.FillColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer))
-        Me.Guna2Button3.Location = New System.Drawing.Point(3, 45)
+        Me.Guna2Button3.Location = New System.Drawing.Point(0, 40)
         Me.Guna2Button3.Name = "Guna2Button3"
         Me.Guna2Button3.PressedColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
         Me.Guna2Button3.PressedDepth = 100
-        Me.Guna2Button3.Size = New System.Drawing.Size(183, 30)
+        Me.Guna2Button3.Size = New System.Drawing.Size(189, 38)
         Me.Guna2Button3.TabIndex = 5
         Me.Guna2Button3.TabStop = False
         Me.Guna2Button3.Text = "Download"
@@ -82,24 +81,23 @@ Partial Class TrackCtxMenu
         '
         'Guna2Button1
         '
-        Me.Guna2Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Guna2Button1.BorderRadius = 4
+        Me.Guna2Button1.Animated = True
         Me.Guna2Button1.CustomImages.Image = Global.EZBeat.My.Resources.Resources.icons8_play_35
         Me.Guna2Button1.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
         Me.Guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray
         Me.Guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
         Me.Guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
         Me.Guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Guna2Button1.FillColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(34, Byte), Integer))
         Me.Guna2Button1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2Button1.ForeColor = System.Drawing.Color.White
         Me.Guna2Button1.HoverState.FillColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer))
-        Me.Guna2Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Guna2Button1.Location = New System.Drawing.Point(0, 0)
         Me.Guna2Button1.Name = "Guna2Button1"
         Me.Guna2Button1.PressedColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
         Me.Guna2Button1.PressedDepth = 100
-        Me.Guna2Button1.Size = New System.Drawing.Size(183, 30)
+        Me.Guna2Button1.Size = New System.Drawing.Size(189, 39)
         Me.Guna2Button1.TabIndex = 3
         Me.Guna2Button1.TabStop = False
         Me.Guna2Button1.Text = "Play"
