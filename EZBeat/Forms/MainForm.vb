f56479eabfcba60a198afb8234cab10d227af151
Jean-Sébastien Grenier <jgrenier0@hotmail.com>
2024-07-09 11:48:03 -0400

    Added support for Play/Pause/Rewind/FastForward to keyboard if application is not focused

--- EZBeat/Forms/MainForm.vb
+++ EZBeat/Forms/MainForm.vb
@@ -2,6 +2,8 @@
 Imports DevExpress.Utils
 Imports DevExpress.XtraEditors
 Imports Microsoft.WindowsAPICodePack.Taskbar
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms
 
 
 Public Class MainForm
@@ -13,6 +15,67 @@ Public Class MainForm
     Public CountSelectedIndex As Integer
     Public CategorieSelectedIndex As Integer
     Public WithEvents player2 As New WAudioPlayer()
+
+    ' Constants for the Windows API functions
+    Private Const WM_HOTKEY As Integer = &H312
+    Private Const MOD_ALT As Integer = &H1
+    Private Const MOD_CONTROL As Integer = &H2
+    Private Const MOD_SHIFT As Integer = &H4
+    Private Const MOD_WIN As Integer = &H8
+
+    ' Import the RegisterHotKey and UnregisterHotKey functions from user32.dll
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function RegisterHotKey(hWnd As IntPtr, id As Integer, fsModifiers As UInteger, vk As UInteger) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function UnregisterHotKey(hWnd As IntPtr, id As Integer) As Boolean
+    End Function
+
+    ' ID for the hotkeys
+    Private Const HOTKEY_ID_PLAY_PAUSE As Integer = 1
+    Private Const HOTKEY_ID_PREV_TRACK As Integer = 2
+    Private Const HOTKEY_ID_NEXT_TRACK As Integer = 3
+
+    ' Virtual key codes for media keys
+    Private Const VK_MEDIA_PLAY_PAUSE As UInteger = &HB3
+    Private Const VK_MEDIA_PREV_TRACK As UInteger = &HB1
+    Private Const VK_MEDIA_NEXT_TRACK As UInteger = &HB0
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If m.Msg = WM_HOTKEY Then
+            Select Case m.WParam.ToInt32()
+                Case HOTKEY_ID_PLAY_PAUSE
+                    ' Handle the Play/Pause button click event
+                    If Panel1.Visible = True Then
+                        If player2.audioplayer.playState = WMPLib.WMPPlayState.wmppsPaused Or player2.audioplayer.playState = WMPLib.WMPPlayState.wmppsStopped Then
+                            player2.audioplayer.controls.play()
+                            PauseBtn.Visible = True
+                            PlayBtn.Visible = False
+                            btnPlayPause.Icon = My.Resources.TaskPause
+                        ElseIf player2.audioplayer.playState = WMPLib.WMPPlayState.wmppsPlaying Then
+                            player2.audioplayer.controls.pause()
+                            PauseBtn.Visible = False
+                            PlayBtn.Visible = True
+                            btnPlayPause.Icon = My.Resources.taskPlay
+                        End If
+                    End If
+                Case HOTKEY_ID_PREV_TRACK
+                    ' Handle the Previous Track action
+                    If player2.audioplayer.controls.currentPosition - 1 >= 0 Then
+                        player2.audioplayer.controls.currentPosition -= 1
+                    End If
+                    TBDuration.Value = player2.audioplayer.controls.currentPosition
+                Case HOTKEY_ID_NEXT_TRACK
+                    ' Handle the Next Track action
+                    If player2.audioplayer.controls.currentPosition + 1 <= player2.audioplayer.currentMedia.duration Then
+                        player2.audioplayer.controls.currentPosition += 1
+                    End If
+                    TBDuration.Value = player2.audioplayer.controls.currentPosition
+            End Select
+        End If
+        MyBase.WndProc(m)
+    End Sub
 
     Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
         Get
@@ -58,13 +121,19 @@ Public Class MainForm
         ' Add the buttons to the taskbar
         TaskbarManager.Instance.ThumbnailToolBars.AddButtons(Me.Handle, btnPrevious, btnPlayPause, btnNext)
 
+        ' Register the Play/Pause media key as a global hotkey
+        RegisterHotKey(Me.Handle, HOTKEY_ID_PLAY_PAUSE, 0, VK_MEDIA_PLAY_PAUSE)
+        ' Register the Previous Track media key as a global hotkey
+        RegisterHotKey(Me.Handle, HOTKEY_ID_PREV_TRACK, 0, VK_MEDIA_PREV_TRACK)
+        ' Register the Next Track media key as a global hotkey
+        RegisterHotKey(Me.Handle, HOTKEY_ID_NEXT_TRACK, 0, VK_MEDIA_NEXT_TRACK)
 
 
     End Sub
 
     Private Sub BtnPrevious_Click(sender As Object, e As ThumbnailButtonClickedEventArgs)
         ' Handle the Previous button click event
-        If textboxFocused = False And Panel1.Visible = True Then
+        If Panel1.Visible = True Then
             If player2.audioplayer.controls.currentPosition - 1 >= 0 Then
                 player2.audioplayer.controls.currentPosition -= 1
             End If
@@ -74,7 +143,7 @@ Public Class MainForm
 
     Private Sub BtnPlayPause_Click(sender As Object, e As ThumbnailButtonClickedEventArgs)
         ' Handle the Play/Pause button click event
-        If textboxFocused = False And Panel1.Visible = True Then
+        If Panel1.Visible = True Then
             If player2.audioplayer.playState = WMPLib.WMPPlayState.wmppsPaused Or player2.audioplayer.playState = WMPLib.WMPPlayState.wmppsStopped Then
                 player2.audioplayer.controls.play()
                 PauseBtn.Visible = True
@@ -91,7 +160,7 @@ Public Class MainForm
 
     Private Sub BtnNext_Click(sender As Object, e As ThumbnailButtonClickedEventArgs)
         ' Handle the Next button click event
-        If textboxFocused = False And Panel1.Visible = True Then
+        If Panel1.Visible = True Then
             If player2.audioplayer.controls.currentPosition + 1 <= player2.audioplayer.currentMedia.duration Then
                 player2.audioplayer.controls.currentPosition += 1
             End If
@@ -199,6 +268,9 @@ Public Class MainForm
 
 
     Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        UnregisterHotKey(Me.Handle, HOTKEY_ID_PLAY_PAUSE)
+        UnregisterHotKey(Me.Handle, HOTKEY_ID_PREV_TRACK)
+        UnregisterHotKey(Me.Handle, HOTKEY_ID_NEXT_TRACK)
         End
     End Sub
 
